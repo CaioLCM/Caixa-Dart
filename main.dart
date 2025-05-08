cf853f54e8c4aa302f5558c4fcbf8d8ed74ec7b0
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-05-08 19:13:00 -0300

    Início do projeto caixa eletrônico

---
+++ main.dart
@@ -0,0 +1,10 @@
+// criar um programa que simule um caixa eletronico 
+// POO
+// FUNÇÕES
+// 3 BRANCHES
+void retornarMenu(){
+  print("MENU CAIXA ELETRONICO\n1- Depósito\n2- Transferência\n3- Fatura");
+}
+main (){
+    retornarMenu();
+}
